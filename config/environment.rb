e66514eb0699d3e9045c237f1a6384ef42f75e06
Sascha Daniels <sd@alternative-solution.de>
2011-02-22 00:42:47 +0100

    production: create data in kamailio dir

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ AUTH_DB_ENGINE = 'dbtext'
 ALIAS_DB_ENGINE = 'dbtext'
 
 if (Rails.env.production?)
-  DBTEXT_SUBSCRIBER_FILE = '/tmp/subscriber'
-  DBTEXT_DBALIASES_FILE = '/tmp/dbaliases'
+  DBTEXT_SUBSCRIBER_FILE = '/opt/kamailio-3.1/etc/kamailio/db_text/subscriber'
+  DBTEXT_DBALIASES_FILE = '/opt/kamailio-3.1/etc/kamailio/db_text/dbaliases'
 elsif (Rails.env.test?)
   DBTEXT_SUBSCRIBER_FILE = '/tmp/subscriber.test'
   DBTEXT_DBALIASES_FILE = '/tmp/dbaliases.test'

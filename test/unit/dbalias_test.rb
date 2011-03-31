3ee7f28679217ac7fd017280b96122e3c016bdcd
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-03-31 14:33:55 +0200

    klarer

--- test/unit/dbalias_test.rb
+++ test/unit/dbalias_test.rb
@@ -1,32 +1,56 @@
 require 'test_helper'
 
 class DbaliasTest < ActiveSupport::TestCase
+  
   should "be valid build" do
     subscriber = Factory.create(:subscriber)
-    assert Factory.build(:dbalias, :username => subscriber.username, :domain => subscriber.domain).valid?
+    assert Factory.build( :dbalias, {
+      :username => subscriber.username,
+      :domain   => subscriber.domain,
+    }).valid?
   end
+  
   should "not be valid with .new" do
-    assert !Dbalias.new.valid?
+    assert ! Dbalias.new.valid?
   end
+  
   should "not be valid with nil alias_username" do
-    assert !Factory.build(:dbalias, :alias_username => nil).valid?
+    assert ! Factory.build( :dbalias, :alias_username => nil ).valid?
   end
+  
   should "not be valid with nil username" do
-    assert !Factory.build(:dbalias, :username => nil).valid?
+    assert ! Factory.build( :dbalias, :username => nil ).valid?
   end
+  
   should "not be valid with nil alias_domain" do
-    assert !Factory.build(:dbalias, :alias_domain => nil).valid?
+    assert ! Factory.build( :dbalias, :alias_domain => nil ).valid?
   end
+  
   should "not be valid with nil domain" do
-    assert !Factory.build(:dbalias, :domain => nil).valid?
+    assert ! Factory.build( :dbalias, :domain => nil ).valid?
   end
-  should "not be valid with username and domain alias_username and alias_domain" do
-    assert !Factory.build(:dbalias, :username => 'test_user', :domain => 'test_domain', :alias_username => 'test_user', :alias_domain => 'test_domain').valid?
+  
+  should "not be valid with alias_username == username and alias_domain == domain" do
+    assert ! Factory.build( :dbalias, {
+      :username       => 'test_user',
+      :domain         => 'test_domain',
+      :alias_username => 'test_user',
+      :alias_domain   => 'test_domain',
+    }).valid?
   end
+  
   should "not be valid if not unique" do
-    subscriber = Factory.create(:subscriber)
-    dbalias = Factory.create(:dbalias, :username => subscriber.username, :domain => subscriber.domain)
-    assert !Factory.build(:dbalias, :username => dbalias.username, :domain => dbalias.domain, :alias_username => dbalias.alias_username, :alias_domain => dbalias.alias_domain).valid?
+    subscriber = Factory.create( :subscriber )
+    dbalias = Factory.create( :dbalias, {
+      :username => subscriber.username,
+      :domain   => subscriber.domain,
+    })
+    assert ! Factory.build( :dbalias, {
+      :username       => dbalias.username,
+      :domain         => dbalias.domain,
+      :alias_username => dbalias.alias_username,
+      :alias_domain   => dbalias.alias_domain,
+    }).valid?
   end
-
+  
 end
